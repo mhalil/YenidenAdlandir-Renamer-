3ed79d5ee44f0095de52e1fd2933cd271b00b91b
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2022-07-03 18:10:58 +0300

    Update Yeniden_Adlandir.py

--- Yeniden_Adlandir.py
+++ Yeniden_Adlandir.py
@@ -1,33 +1,78 @@
 import os
 
-dizin = input("Yeniden adlandirilacak dosyalarin bulunduğu dizin yolunu belirtin: ")
+# Fonksiyonlar
+def sirali():
+    Yeni_Dosya_Adi = input("Dosyalara Verilecek Yeni Adı Belirtin: ")
 
-if dizin == "":
-    dizin = "/home/halil/Belgeler/Yeniden_Adlandir"
+    sayac_1 = 1
+    yeni_dosya_adi_listesi = []
 
-dosya_klasor_adlari = os.listdir(dizin)
-dosya_adlari = []
+    for dosya_adi in dosya_adlari:
+        uzanti_indeksi = dosya_adi.rfind(".")   # dosya uzantisinin indis değerini tespit ettim.
+        uzanti = dosya_adi[uzanti_indeksi:]     # dosya uzantisini aldım
+        yeni_dosya_adi_listesi.append(Yeni_Dosya_Adi + str(sayac_1) + uzanti)
+        sayac_1 += 1
 
-for isim in dosya_klasor_adlari:
-    if isim.count(".") > 0:         # sadece dosya isimlerini almak için olusturalan kosul, klasor isimlerini alma
-        dosya_adlari.append(isim)
+    sayac_2 = 0
+    for ad in dosya_adlari:
+        os.rename(dizin+"/"+ad, dizin+"/"+yeni_dosya_adi_listesi[sayac_2])
+        sayac_2 += 1
 
-Yeni_Dosya_Adi = input("Dosyalara Verilecek Yeni Adı Belirtin: ")
+    print("Dosyalar Yeniden İsimlendirildi. Dosyaların Yeni İsimleri;")
+    for isim in yeni_dosya_adi_listesi:
+        print(isim)
 
-sayac_1 = 1
-yeni_dosya_adi_listesi = []
+def onEk():
+    pass
 
-for dosya_adi in dosya_adlari:
-    uzanti_indeksi = dosya_adi.rfind(".")   # dosya uzantisinin indis değerini tespit ettim.
-    uzanti = dosya_adi[uzanti_indeksi:]     # dosya uzantisini aldım
-    yeni_dosya_adi_listesi.append(Yeni_Dosya_Adi + str(sayac_1) + uzanti)
-    sayac_1 += 1
+def sonEk():
+    pass
 
-sayac_2 = 0
-for ad in dosya_adlari:
-    os.rename(dizin+"/"+ad, dizin+"/"+yeni_dosya_adi_listesi[sayac_2])
-    sayac_2 += 1
+def buyukHarf():
+    pass
 
-print("Dosyalar Yeniden İsimlendirildi. Dosyaların isimleri;")
-for isim in yeni_dosya_adi_listesi:
-    print(isim)
+def kucukHarf():
+    pass
+
+def karakterSil():
+    pass
+
+def kelimeDegistir():
+    pass
+
+def kelimeSil():
+    pass
+
+#Menü
+menu = {"1": sirali, "2": onEk, "3": sonEk, "4": buyukHarf, "5": kucukHarf, "6": karakterSil, "7": kelimeDegistir, "8": kelimeSil}
+print("***** MENÜ *****\n1: Sıralı Artır (Ör. resim1.png, resim2.png, ...)\n2: Ön Ek Ekle\n3: Son Ek Ekle\n4: Büyük Harfe Çevir\n5: Küçük Harfe Çevir\n6: Karakter Sil (Ör. boşluk, -)\n7: Kelime Değiştir\n8: Kelime Sil")
+
+# Dosyayı çalıştır, Menüyü görüntüle, seçime göre işlem/fonksiyon çalıştır
+while True:
+    try:
+        # Menü Seçeneklerinden birini Seçin
+        islem = input("Menü seçeneklerinden birini belirtin: ")
+
+        #Dizin Secimi
+        dizin = input("Çalışma dizininin konumunu belirtin: ")  # Yeniden adlandırılmasını istediğiniz dosyaların bulunduğu klasöün bulunduğu dizin/yol
+
+        # Varsayılan dizin yolu (Kod esnasinda zamandan kazanmak için)
+        if dizin == "":
+            dizin = "/home/halil/Belgeler/Yeniden_Adlandir"
+
+        # İlgili dizindeki tüm Dosya ve Klasör isimleri
+        dosya_klasor_adlari = os.listdir(dizin)
+
+        # Yalnız Dosya adlarının barındırılacağı liste
+        dosya_adlari = []
+
+        # Yalnız Dosya isimlerini seç ve "dosya_adlari" listesine ekle
+        for isim in dosya_klasor_adlari:
+            if isim.count(".") > 0:         # sadece dosya isimlerini almak için olusturalan kosul, klasor isimlerini alma
+                dosya_adlari.append(isim)
+
+        menu[islem]()
+
+    except KeyError:
+        print("Geçersiz Seçim") 
+        break
