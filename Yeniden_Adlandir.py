f2e9a612e514e02edc1cd78f7d815fc95efba68f
Mustafa Halil <80210006+mhalil@users.noreply.github.com>
2022-07-03 16:32:50 +0300

    Update Yeniden_Adlandir.py

--- Yeniden_Adlandir.py
+++ Yeniden_Adlandir.py
@@ -10,25 +10,24 @@ import os
 # print(dizin)
 
 dizin = os.getcwd()
+dosya_adlari = os.listdir(dizin)
 
+# print(dosya_adlari)
 Yeni_Dosya_Adi = input("Dosyalara Verilecek Yeni Adı Belirtin: ")
 
 sayac_1 = 1
 yeni_dosya_adi_listesi = []
 
-for dosya_adi in dizin:
+for dosya_adi in dosya_adlari:
     if (dosya_adi.count(".") >=1 ):     # sadece dosyaları seçmek için bu kontrolü yazdım, Klasörleri adlandırmayacağım.
         uzanti_indeksi = dosya_adi.rfind(".")   # dosya uzantisinin indis değerini tespit ettim.
         uzanti = dosya_adi[uzanti_indeksi:]     # dosya uzantisini aldım
         yeni_dosya_adi_listesi.append(Yeni_Dosya_Adi + str(sayac_1) + uzanti)
         sayac_1 += 1
 
-print(yeni_dosya_adi_listesi)
+# print(yeni_dosya_adi_listesi)
 
-# sayac_2 = 0
-# for i in dizin:
-#     if (i.count(".") >=1 ):     # sadece dosyaları seçmek için bu kontrolü yazdım, Klasörleri adlandırmayacağım.
-#         os.rename(dizin[sayac_2], yeni_dosya_adi_listesi[sayac_2])
-#         sayac_2 += 1
-
-
+sayac_2 = 0
+for ad in dosya_adlari:
+    os.rename(ad, yeni_dosya_adi_listesi[sayac_2])
+    sayac_2 += 1
